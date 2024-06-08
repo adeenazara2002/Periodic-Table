8d948194bf787ba5e624c963d7da39dd73058524
adeenazara2002 <adinazara2001@gmail.com>
2024-06-08 18:24:27 +0500

    Periodic Table

---
+++ lib/pt.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class PeriodicTable extends StatelessWidget {
+  const PeriodicTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text(
+            'Periodic Table',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.black,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                // mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 855.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                ],
+              ),
+
+              // Second row
+
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    // padding:EdgeInsets.all(18.0) ,
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green),
+                    ),
+                    height: 40,
+                    width: 45,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
